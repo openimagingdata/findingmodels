de70aa86ffd0e8fc2a0b60fc1d6b2b4d74b6d6e7
vijaydawal <vijaydawal@microsoft.com>
2025-06-02 10:04:32 -0400

    testing pre-commit script

---
+++ scripts/generate_index_jsonl.py
@@ -0,0 +1,19 @@
+from findingmodel.index import Index
+from pathlib import Path
+import subprocess
+
+
+def main():
+    script_dir = Path(__file__).parent
+    project_root = script_dir.parent
+    output_file = project_root / "index1.jsonl"
+    index = Index(base_directory=project_root)
+    index.populate_from_directory(project_root)
+    index.export_to_jsonl(output_file)
+
+    # Add the generated file to git
+    subprocess.run(["git", "add", output_file], check=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/generate_index_markdown.py
@@ -0,0 +1,35 @@
+from findingmodel.index import Index
+from pathlib import Path
+
+
+def main():
+    script_dir = Path(__file__).parent
+    project_root = script_dir.parent
+    output_file = project_root / "index1.md"
+    index = Index(base_directory=project_root)
+    markdown = to_markdown(index)
+    output_file.write_text(markdown)
+
+
+def to_markdown(self) -> str:
+    """Converts the index to a Markdown table."""
+    header = f"""
+# Finding Model Index
+
+{self.__len__()} entries
+
+| ID | Name | Synonyms | Tags | Contributors | Attributes |\n"""
+    separator = "|----|------|----------|------|--------------|------------|\n"
+    rows = []
+    for entry in sorted(self.entries, key=lambda e: e.name.casefold()):
+        md_filename = entry.filename.replace(".fm.json", ".md")
+        entry_name_with_links = (
+            f"[{entry.name}](text/{md_filename}) [JSON](defs/{entry.filename})"
+        )
+        row = f"| {entry.oifm_id} | {entry_name_with_links} | {', '.join(entry.synonyms or [])} | {', '.join(entry.tags or [])} | {', '.join(entry.contributors or [])} | {', '.join(attr.name for attr in entry.attributes)} |\n"
+        rows.append(row)
+    return header + separator + "".join(rows)
+
+
+if __name__ == "__main__":
+    main()
